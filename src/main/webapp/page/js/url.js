44b7d3805d51f9acfa3da8f82f03f4494ab5da40
0jinyun <1219633494@qq.com>
2019-01-10 15:39:23 +0800

    登录区分大小片区经理，协议书查询修改无数据bug

--- src/main/webapp/page/js/url.js
+++ src/main/webapp/page/js/url.js
@@ -2,5 +2,6 @@
 // var remoteURL="http://localhost:80/yulan";
 // var remoteURL="http://localhost:8080/yulan";
 var remoteImageURL = "http://localhost:80/upload";
+var remoteURL="http://localhost:8080/yulan";
 //远程服务器
-var remoteURL="http://119.23.17.111:8081/yulan";
+// var remoteURL="http://119.23.17.111:8081/yulan";
