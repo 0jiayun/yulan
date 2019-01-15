06343ce7747e16acda38392f55e21a3b2ba44fb7
BlueVegetable <2601042086@qq.com>
2019-01-16 02:51:14 +0800

    修改数据库配置

--- src/test/UserTest.java
+++ src/test/UserTest.java
@@ -1,18 +1,31 @@
+import com.yulan.dao.CustomerInfoCardDao;
+import com.yulan.pojo.CustomerInfoCard;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class UserTest {
+    @Autowired
+    private CustomerInfoCardDao customerInfoCardDao;
     @Test
     public void test1() {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒");
-        System.out.println(simpleDateFormat.format(new Date(System.currentTimeMillis())));
+        List<CustomerInfoCard> customerInfoCards = new ArrayList<>();
+        CustomerInfoCard customerInfoCard = new CustomerInfoCard();
+        customerInfoCard.setGroupid("3078e1a17ffa40cfb81cf42da0f4ce23");
+        customerInfoCard.setCid("FNR002");
+        customerInfoCards.add(customerInfoCard);
+        CustomerInfoCard customerInfoCard1 = new CustomerInfoCard();
+        customerInfoCard1.setGroupid("3078e1a17ffa40cfb81cf42da0f4ce23");
+        customerInfoCard1.setCid("FNR006");
+        customerInfoCards.add(customerInfoCard1);
+//        customerInfoCardDao.addCustomerInfoCards(customerInfoCards);
     }
 }
 
